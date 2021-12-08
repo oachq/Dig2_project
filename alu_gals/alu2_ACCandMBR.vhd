f996b4bc56770c8461a846457766587989d248c4
oachq <alan_oscar52@hotmail.com>
2021-12-08 14:24:24 -0700

    gals

--- alu_gals/alu2_ACCandMBR.vhd
+++ alu_gals/alu2_ACCandMBR.vhd
@@ -30,14 +30,19 @@ entity alu is
 end alu ; 
 
 architecture arch of alu is
-    signal cuenta :         std_logic_vector(wl downto 0);
+    signal cuenta :         std_logic_vector(wl-1 downto 0);
     --signal corrimiento: std_logic_vector(wl downto 0);
 begin
     process(a_in, b_in, s, cuenta )
     begin
         if (s = "001") then --ACC and MBR  
             cuenta <= a_in and b_in;
-            c <= cuenta(8);
+            c <= '0';
+            if (cuenta ="111111110") then
+                c='1';
+            else
+                c='0';
+            end if ;
         end if ;
     if (cuenta = "00000000") then
         z <= '1';
@@ -46,4 +51,5 @@ begin
     end if ;
 end process;  
 	  F <= cuenta(wl-1 downto 0) ;
+      --c <= cuenta(8)
 end architecture ;
