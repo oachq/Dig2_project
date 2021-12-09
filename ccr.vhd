ee1d1955311bf55943cd6601a0089d25a9e2532c
oachq <alan_oscar52@hotmail.com>
2021-12-08 17:26:35 -0700

    gals

--- ccr.vhd
+++ ccr.vhd
@@ -1,37 +1,43 @@
-library ieee ;
-    use ieee.std_logic_1164.all ;
-    use ieee.numeric_std.all ;
-    use IEEE.std_logic_arith.all;
-    use IEEE.std_logic_unsigned.all;
-    
-entity reg_CCR is
-  port (
-    clk:        in std_logic;
-    ccr_in:    in std_logic_vector(1 downto 0); -- salida alu / entrada ccr
-    ccr_ce:     in std_logic; 
-    ccr_arst:   in std_logic;
-    ccrC_out:   out std_logic;
-    ccrZ_out:   out std_logic
-  ) ;
-end reg_CCR ; 
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-architecture arch of reg_CCR is
-    signal flags : std_logic_vector(1 downto 0); 
-   
+entity CCR is
+    Port ( CCRCIn : in  STD_LOGIC;
+	        CCRZIn : in  STD_LOGIC;
+           CE : in  STD_LOGIC; 
+           CLK : in  STD_LOGIC;
+			  RESET : in  STD_LOGIC; --Reset Asincrono
+			  CCRCOut : out  STD_LOGIC;
+           CCRZOut : out  STD_LOGIC); 
+end CCR;
+
+architecture Behavioral of CCR is
+	SIGNAL TempCCRCOut : STD_LOGIC := '0';
+	SIGNAL TempCCRZOut : STD_LOGIC := '0';
 begin
-    process (clk, ccr_ce, ccr_arst)
-    begin
-        if (crr_ce='1') then
-            if (ccr_arst = '1') then
-                if rising_edge(clk) then
-                    flags <= ccr_in;
-                    ccrC_out <= ccr_in(1);
-                end if ;
-            else
-                flags <= "00";
-            end if ;
-        end if ;
-    end process;
-    ccrC_out <= flags(1);
-    ccrZ_out <= flags(0); 
-end architecture ;
+	CCRCOut <= TempCCRCOut;
+	CCRZOut <= TempCCRZOut;
+	
+	PROCESS (CLK, CCRCIn,CCRZIn , CE, RESET) 
+	BEGIN
+		CASE RESET IS
+			WHEN '1' =>
+				TempCCRCOut <= '0';
+				TempCCRZOut <= '0';
+				
+			WHEN OTHERS =>
+				IF RISING_EDGE(CLK) THEN
+				IF (CE = '1') THEN
+					TempCCRCOut <= CCRCIn;
+					TempCCRZOut <= CCRZIn;
+				ELSE
+					TempCCRCOut <= TempCCRCOut;
+					TempCCRZOut <= TempCCRZOut;
+				END IF;
+			   END IF;
+			END CASE;
+		END PROCESS;
+end Behavioral;
+
