2933270d23e68dfe47cfa4e8966d27dc41f9cc5a
oachq <alan_oscar52@hotmail.com>
2021-12-08 17:42:51 -0700

    gals

--- ccr.vhd
+++ ccr.vhd
@@ -1,40 +1,37 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee ;
+    use ieee.std_logic_1164.all ;
+    use ieee.numeric_std.all ;
+    use IEEE.std_logic_arith.all;
+    use IEEE.std_logic_unsigned.all;
+    
+entity reg_CCR is
+  port (
+    clk:        in std_logic;
+    ccr_in:    in std_logic_vector(1 downto 0); -- salida alu / entrada ccr
+    ccr_ce:     in std_logic; 
+    ccr_arst:   in std_logic;
+    ccrC_out:   out std_logic;
+    ccrZ_out:   out std_logic
+  ) ;
+end reg_CCR ; 
 
-entity ccr is
-port(
-
-sal_alu: in std_logic_vector (1 downto 0);--- recibe de la alu 2 bits
-ce: 	 in std_logic;--- linea de activacion
-flags: OUT std_logic_vector (1 downto 0);--- manda las flags ala UC
-cout:	 OUT std_logic; ---- Carry de salida
-clk: 	in std_logic;
-arst:	in std_logic---- pos el reset
-
-);
-end ccr;
-
-
-architecture behavioral of ccr is
+architecture arch of reg_CCR is
+    --signal flags : std_logic_vector(1 downto 0); 
+   
 begin
-	process(clk,arst,ce)
-	begin
-	if ce='1' then
-			if arst='1' then
-				if rising_edge(clk) then
-					flags<=sal_alu;
-					cout<=sal_alu(1);
+    process (clk)
+    begin
+        if (resing_edge(clk)) then
+            if (ccr_arst = '1') then
+                if (ccr_ce = '1') then
+                ccrC_out <= '0';
+                ccrZ_out <= '0';
+                end if ;
+            else
+            ccrC_out <= ccr_in(1);
+            ccrZ_out <= ccr_in(0);
+            end if ;
+        end if ;
 
-				end if;
-				else
-				flags<="00";
-				cout<='0';
-
-			end if;
-			end if;
-
-
-	end process;
-	end behavioral;
+    end process;
+end architecture ;
